7addf183959bf5135ca5fcf5cbdc610e787f679f
Cole Gottdank <colegottdank@gmail.com>
2019-07-13 19:21:02 -0400

    fix

--- DataGrabber.py
+++ DataGrabber.py
@@ -1,29 +1,18 @@
 import alpaca_trade_api as tradeapi
-from api import ColeApi
+from api import *
 import os
 import csv
 
-os.environ["APCA_API_BASE_URL"] = "https://api.alpaca.markets"
-api = tradeapi.REST('AKBHEJ0EK9IO98U9DB8K', 'xXdnDTWWyExwkL7jrwsIOiOawnfaUQKVSykMBNk7')
+api = tradeapi.REST(get_api_key(), get_secret_key())
 account = api.get_account()
-
 storageLocation = "./Data/"
 barTimeframe = "1D"  # 1Min, 5Min, 15Min, 1H, 1D
 assetsToDownload = ["SPY", "MSFT", "AAPL", "NFLX", "DOW", "TSLA"]
 
-for a in assetsToDownload:
-    symbol = a
-
+for symbol in assetsToDownload:
     dataFile = ""
-    lastDate = "2013-00-00T00:00:00.000Z"  # ISO8601 Date
-
-    # Verifies if symbol file exists
-    try:  # If file exists, reads the time of the last bar
-        dataFile = open(storageLocation + '{0}.csv'.format(symbol), 'a+')
-        lastDate = list(csv.DictReader(dataFile))[-1]["time"]
-    except:  # If not, initialises new CSV file
-        dataFile = open(storageLocation + '{0}.csv'.format(symbol), 'w')
-        dataFile.write("day,open,high,low,close,volume\n")
+    dataFile = open(storageLocation + '{0}.csv'.format(symbol), 'w')
+    dataFile.write("day,open,high,low,close,volume\n")
 
     returned_data = api.polygon.historic_agg("day", symbol, limit=10)
 
@@ -37,7 +26,6 @@ for a in assetsToDownload:
         ret_volume = str(day.volume)
 
         # Writes formatted line to CSV file
-        # dataFile.write(ret_time + "," + ret_open + "," + ret_high + "," + ret_low + "," + ret_close + "," + ret_volume)
         dataFile.write(ret_day + "," + ret_open + "," + ret_high + "," + ret_low + "," + ret_close + "," + ret_volume + "\n")
 
     dataFile.close()
--- api.py
+++ api.py
@@ -2,12 +2,9 @@ import requests
 import json
 
 
-class ColeApi:
-    def __init__(self, url, key, secret, version):
-        self.url = url
-        self.key = key
-        self.secret = secret
-        self.version = version
+def get_api_key():
+    return "AKBHEJ0EK9IO98U9DB8K"
 
-    def get_historical_data(self):
-        requests.get("https://api.polygon.io/v1/last/stocks/AAPL?apiKey=AKBHEJ0EK9IO98U9DB8K")
+
+def get_secret_key():
+    return "xXdnDTWWyExwkL7jrwsIOiOawnfaUQKVSykMBNk7"
